c6297108a04d2d68614d81544db8a6ff6864977e
Awsome-Creeper9 <gherdt@outlook.com>
2025-11-06 21:44:43 -0600

    Initial commit (Vanilla and Minesweeper)

---
+++ scripts/modselect.js
@@ -0,0 +1,63 @@
+let mods = {}
+
+setInterval(() => {
+    let modSelected = document.getElementById("ModSelect").value
+    document.querySelectorAll(".modDiv").forEach((e) => {
+        if (modSelected + "Div" !== e.id) {
+            e.style.display = "none";
+        }
+        else {
+            e.style.display = "unset";
+        }
+    })
+    document.querySelectorAll(".modCB").forEach((e) => {
+        if (e.checked) {
+            mods[e.id.slice(3, e.id.length)] = true
+        }
+        else {
+            mods[e.id.slice(3, e.id.length)] = false
+        }
+    })
+
+    if (modSelected !== "Select Mod...") {
+        createCB(modSelected)
+    }
+
+    showPages()
+},500)
+
+function createCB(mod) {
+    if (!document.getElementById(mod+"Div")) {
+        let modDiv = document.createElement("div")
+        modDiv.id = mod + "Div"
+        modDiv.classList = "modDiv"
+
+        let newline = document.createElement("br")
+
+        let checkbox = document.createElement("input")
+        checkbox.type = "checkbox"
+        checkbox.id = "CB_" + mod
+        checkbox.classList = "modCB"
+        let label = document.createElement("label")
+        label.id = mod + "Label"
+        label.classList = "modLabel"
+        label.for = checkbox.id
+        label.innerText = `Enable ${mod}:`
+
+        modDiv.appendChild(newline)
+        modDiv.appendChild(label)
+        modDiv.appendChild(checkbox)
+        document.getElementById("ModSelectDiv").appendChild(modDiv)
+    }
+}
+
+function showPages() {
+    document.querySelectorAll(".Minesweeper").forEach((e) => {
+        if (mods.Minesweeper) {
+            e.style.display = "unset"
+        }
+        else {
+            e.style.display = "none"
+        }
+    })
+}
